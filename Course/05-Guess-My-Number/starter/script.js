1cdfc85131ab53de92bbc7f9381e3bdf08edc2d5
TareqJudehGithub <tareq.joudeh@gmail.com>
2025-07-13 19:32:43 +0300

    Completed Project#1: Guessing The Number. Where the system rolls a random number, and user has to guess it under limited attempts.

--- Course/05-Guess-My-Number/starter/script.js
+++ Course/05-Guess-My-Number/starter/script.js
@@ -1,30 +1,72 @@
 'use strict';
 
+//let number = document.querySelector('.number');
+
 let message = document.querySelector('.message');
-console.log(message);
-
 const scoreTitle = document.querySelector('.label-score').textContent;
-const score = document.querySelector('.score').textContent;
-const number = (document.querySelector('.number').textContent = '13');
-
+let score = document.querySelector('.score');
+let highscore = document.querySelector('.highscore');
 let input = document.querySelector('.guess');
+const againBtn = document.querySelector('.again');
 
 // Check button
 let check = document.querySelector('#check');
+highscore.textContent;
 
+// Secret number
+function rollSecretNum() {
+  const secretNum = Math.trunc(Math.random() * 20 + 1);
+  return secretNum;
+  //(number.textContent = String(secretNum));
+}
+let secretNum = rollSecretNum();
+
+score.textContent = '20';
+highscore.textContent = '0';
+
+let scoreNum = 20;
+let highscoreNum = 0;
+
+console.log(`Highscore = ${highscoreNum}`);
+console.log(`Secret Number: ${secretNum}`);
+
+// Events
 // Check input field saved in a variable
 check.addEventListener('click', function () {
   const guessValue = Number(input.value);
-  // if no input
+
+  // Check button event
   if (!guessValue) {
     message.textContent = 'No number entered!';
+    console.log('No number entered!');
+  } else if (secretNum === guessValue) {
+    message.textContent = `It is indeed ${secretNum}`;
+    console.log(`Correct! The secret number is indeed ${secretNum}!`);
+    highscoreNum++;
+    highscore.textContent = String(highscoreNum);
+    console.log(highscore.textContent);
+  } else if (guessValue < secretNum) {
+    message.textContent = `You guessed lower than the Secret Number!`;
+    scoreNum--;
+    score.textContent = String(scoreNum);
+  } else if (guessValue > secretNum) {
+    message.textContent = `You guessed higher than the Secret Number!`;
+    scoreNum--;
+    score.textContent = String(scoreNum);
   } else {
-    console.log(guessValue, typeof guessValue);
+    message.textContent = 'Please try again.';
+    console.log(`${guessValue} is not the secret number :(\nPlease try again.`);
   }
+  if (scoreNum === 0) {
+    message.textContent = 'Game Over!';
+  }
+  console.log(`Attempts left: ${score.textContent}`);
 });
 
-console.log(scoreTitle);
-console.log(score);
-console.log(number);
-
-// console.log(input.value);
+// again button (reset game) event
+againBtn.addEventListener('click', function () {
+  secretNum = rollSecretNum();
+  console.log(`Secret Number: ${secretNum}`);
+  score.textContent = '20';
+  message.textContent = 'Start guessing...';
+});
