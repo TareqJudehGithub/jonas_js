63c2f7827d42f185fe3ce149550df2eca8e659d3
TareqJudehGithub <tareq.joudeh@gmail.com>
2025-07-14 20:45:28 +0300

    Completed Project Show/Hide HTML element, where we got to apply and practice DOM .Classlist method and some of the properties it contains.

--- Course/05-Guess-My-Number/starter/script.js
+++ Course/05-Guess-My-Number/starter/script.js
@@ -1,13 +1,15 @@
 'use strict';
 
-//let number = document.querySelector('.number');
-
+// Game logic variables
+let number = document.querySelector('.number');
 let message = document.querySelector('.message');
 const scoreTitle = document.querySelector('.label-score').textContent;
 let score = document.querySelector('.score');
 let highscore = document.querySelector('.highscore');
 let input = document.querySelector('.guess');
 const againBtn = document.querySelector('.again');
+
+// CSS variables
 
 // Check button
 let check = document.querySelector('#check');
@@ -17,7 +19,6 @@ highscore.textContent;
 function rollSecretNum() {
   const secretNum = Math.trunc(Math.random() * 20 + 1);
   return secretNum;
-  //(number.textContent = String(secretNum));
 }
 let secretNum = rollSecretNum();
 
@@ -36,20 +37,36 @@ check.addEventListener('click', function () {
   const guessValue = Number(input.value);
 
   // Check button event
+  // No entry
   if (!guessValue) {
     message.textContent = 'No number entered!';
     console.log('No number entered!');
-  } else if (secretNum === guessValue) {
+  }
+  // Player wins
+  else if (secretNum === guessValue) {
+    // Change bg and font size
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').computedStyleMap.width = '30rem';
     message.textContent = `It is indeed ${secretNum}`;
+
+    number.textContent = String(secretNum);
     console.log(`Correct! The secret number is indeed ${secretNum}!`);
-    highscoreNum++;
-    highscore.textContent = String(highscoreNum);
+
+    if (scoreNum > highscoreNum) {
+      highscoreNum = scoreNum;
+      highscore.textContent = String(highscoreNum);
+    }
+
     console.log(highscore.textContent);
-  } else if (guessValue < secretNum) {
+  }
+  // Guessed number lower than secret number
+  else if (guessValue < secretNum) {
     message.textContent = `You guessed lower than the Secret Number!`;
     scoreNum--;
     score.textContent = String(scoreNum);
-  } else if (guessValue > secretNum) {
+  }
+  // Guessed number higher than secret number
+  else if (guessValue > secretNum) {
     message.textContent = `You guessed higher than the Secret Number!`;
     scoreNum--;
     score.textContent = String(scoreNum);
@@ -57,6 +74,7 @@ check.addEventListener('click', function () {
     message.textContent = 'Please try again.';
     console.log(`${guessValue} is not the secret number :(\nPlease try again.`);
   }
+  // No more attempts left
   if (scoreNum === 0) {
     message.textContent = 'Game Over!';
   }
@@ -65,8 +83,14 @@ check.addEventListener('click', function () {
 
 // again button (reset game) event
 againBtn.addEventListener('click', function () {
+  number.textContent = '?';
   secretNum = rollSecretNum();
   console.log(`Secret Number: ${secretNum}`);
+  scoreNum = 20;
   score.textContent = '20';
   message.textContent = 'Start guessing...';
+  input.value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').computedStyleMap.width = '15rem';
+  console.log(scoreNum);
 });
--- Course/06-Modal/starter/script.js
+++ Course/06-Modal/starter/script.js
@@ -1,1 +1,34 @@
-'use strict';
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnShowModal = document.querySelectorAll(".show-modal");
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+// Iterate over all elements with class "show-modal" and access the textContent property.
+for (let i = 0; i < btnShowModal.length; i++) {
+  // Open menu
+  btnShowModal[i].addEventListener("click", openModal);
+}
+
+// Show hidden elements - class hidden - Close menu
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+// "keydown" is triggered by pressing 'any' key on the keyboard.
+document.addEventListener("keydown", function (event) {
+  console.log(event.key);
+  // if Escape key was pressed and the modal is currently not hidden
+  if (event.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
