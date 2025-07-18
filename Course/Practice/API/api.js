324beca486b13b9b6544bf44ecd9dca597353b6f
TareqJudehGithub <tareq.joudeh@gmail.com>
2025-07-19 00:52:25 +0300

    Completed API promises and async/await lessons.

--- Course/Practice/API/api.js
+++ Course/Practice/API/api.js
@@ -2,7 +2,7 @@
       be executed after a task in the background finishes execution.
 
     Asynchronous is non-blocking. 
-    Execution doesn't wait for an asynchronous task to finish its work.
+     Execution doesn't wait for an asynchronous task to finish its work.
 
 
  */
@@ -10,6 +10,25 @@
 console.log("Line 1");
 
 setTimeout(() => {
-	console.log("Line 2");
+  console.log("Line 2");
 }, 3000);
 console.log("Line 3");
+
+/* *******************************************************************/
+
+console.log("\n\n");
+// Promises
+fetch("https://jsonplaceholder.typicode.com/users") // returns a promise
+  .then((res) => res.json()) // returns another promise
+  .then((data) => console.log(data)); // returns all data
+
+console.log("synchronous");
+console.log("\n\n");
+
+// async await
+async function fetchData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await res.json();
+  console.log(res);
+}
+fetchData();
